d35168f4d44ff3df6c1ad9f082d91977e9818ed0
Ronaldo Cipriano <ronaldolopes14@gmail.com>
2024-04-02 11:28:55 -0300

    Feat: Implementando páginda de detalhes de uma noticia

--- news/views.py
+++ news/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from news.models import News
 
 
@@ -7,3 +7,10 @@ def home(request):
         'news': News.objects.all()
     }
     return render(request, 'home.html', context)
+
+
+def news_details(request, id):
+    context = {
+        'news': get_object_or_404(News, id=id)
+    }
+    return render(request, 'news_details.html', context)
