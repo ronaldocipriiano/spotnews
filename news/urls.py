86c5eb7670d0a2c17094809ecb2731ba4c5f6405
Ronaldo Cipriano <ronaldolopes14@gmail.com>
2024-04-02 12:24:04 -0300

    Feat: Implementando a pagina de uma nova categoria

--- news/urls.py
+++ news/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from news.views import home, news_details
+from news.views import home, news_details, categories_form
 
 
 urlpatterns = [
     path('', home, name='home-page'),
-    path('news/<int:id>', news_details, name='news-details-page')
+    path('news/<int:id>', news_details, name='news-details-page'),
+    path('categories', categories_form, name='categories-form')
 ]
--- news/views.py
+++ news/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
-from news.models import News
+from django.shortcuts import render, get_object_or_404, redirect
+from news.forms import CategoryForm
+from news.models import News, Category
 
 
 def home(request):
@@ -14,3 +15,17 @@ def news_details(request, id):
         'news': get_object_or_404(News, id=id)
     }
     return render(request, 'news_details.html', context)
+
+
+def categories_form(request):
+    form = CategoryForm()
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+
+        if form.is_valid():
+            Category.objects.create(**form.cleaned_data)
+        return redirect('home-page')
+    context = {
+        'form': form
+    }
+    return render(request, 'categories_form.html', context)
